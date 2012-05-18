7e8add3e1e67110654a5e78363a07370153e36f1
Emil Stenqvist <emsten@gmail.com>
2012-05-18 17:19:42 +0200

    [cleanup] Lotsa cleanup

--- wip/jquery-navigate.js
+++
@@ -1,6 +0,0 @@
-$('input[id="auth:kundnummer"]').val('8707017177')
-$('select[id="auth:metod_2"]').val('PIN6')
-$('input[id="auth:fortsett_knapp"]').click()
-$('input[id="form:pinkod"]').val('x4395Q')
-$('input[id="form:fortsett_knapp"]').click()
-
